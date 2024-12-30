36e601ef88e3092d2c575e84e99a4f752485e054
Brian Lukonsolo <brian.lukonsolo@gmail.com>
2024-12-30 16:09:17 +0000

    feat(*): use jks instead of p12 successfully

--- certs/generate-certs.sh
+++ certs/generate-certs.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-# Generate microservice certificate
-keytool -genkeypair -alias microservice -keyalg RSA -keysize 2048 -storetype PKCS12 -keystore microservice-keystore.p12 -validity 3650 -storepass password -dname "CN=localhost, OU=Microservice, O=BrianLukonsolo, L=City, ST=State, C=Country" -ext SAN=dns:localhost,ip:127.0.0.1
+# Generate microservice
+keytool -genkeypair -alias microservice -keyalg RSA -keysize 2048 -storetype PKCS12 -keystore microservice-keystore.jks -validity 3650 -storepass password -dname "CN=localhost, OU=Microservice, O=BrianLukonsolo, L=City, ST=State, C=Country" -ext SAN=dns:localhost,ip:127.0.0.1
 
 # Generate WireMock certificate
-keytool -genkeypair -alias wiremock -keyalg RSA -keysize 2048 -storetype PKCS12 -keystore wiremock-keystore.p12 -validity 3650 -storepass password -dname "CN=localhost, OU=WireMock, O=BrianLukonsolo, L=City, ST=State, C=Country" -ext SAN=dns:localhost,ip:127.0.0.1
+keytool -genkeypair -alias wiremock -keyalg RSA -keysize 2048 -storetype PKCS12 -keystore wiremock-keystore.jks -validity 3650 -storepass password -dname "CN=localhost, OU=WireMock, O=BrianLukonsolo, L=City, ST=State, C=Country" -ext SAN=dns:localhost,ip:127.0.0.1
 
 # Export microservice certificate
-keytool -exportcert -alias microservice -keystore microservice-keystore.p12 -storepass password -file microservice.cer
+keytool -exportcert -alias microservice -keystore microservice-keystore.jks -storepass password -file microservice.cer
 
 # Export WireMock certificate
-keytool -exportcert -alias wiremock -keystore wiremock-keystore.p12 -storepass password -file wiremock.cer
+keytool -exportcert -alias wiremock -keystore wiremock-keystore.jks -storepass password -file wiremock.cer
 
 # Import WireMock certificate into microservice keystore
-keytool -importcert -alias wiremock -file wiremock.cer -keystore microservice-keystore.p12 -storepass password -noprompt
+keytool -importcert -alias wiremock -file wiremock.cer -keystore microservice-keystore.jks -storepass password -noprompt
 
 # Import microservice certificate into WireMock keystore
-keytool -importcert -alias microservice -file microservice.cer -keystore wiremock-keystore.p12 -storepass password -noprompt
+keytool -importcert -alias microservice -file microservice.cer -keystore wiremock-keystore.jks -storepass password -noprompt
 
 # Clean up temporary certificate files
 rm microservice.cer wiremock.cer
 
 # Copy certificates to certs volume directory
-cp ./*.p12 /certs/
+cp ./*.jks /certs/
--- wiremock.Dockerfile
+++ wiremock.Dockerfile
@@ -7,8 +7,8 @@ WORKDIR /wiremock
 RUN apt-get update && apt-get install -y wget && \
     wget https://repo1.maven.org/maven2/org/wiremock/wiremock-standalone/3.10.0/wiremock-standalone-3.10.0.jar -O wiremock.jar
 
-COPY /certs/*.p12 /wiremock/certs/
+COPY /certs/*.jks /wiremock/certs/
 
 EXPOSE 9091
 
-CMD ["java", "-jar", "wiremock.jar", "--https-port", "9091", "--https-keystore", "/wiremock/certs/wiremock-keystore.p12", "--keystore-password", "password", "--root-dir", "/wiremock"]
+CMD ["java", "-jar", "wiremock.jar", "--https-port", "9091", "--https-keystore", "/wiremock/certs/wiremock-keystore.jks", "--keystore-password", "password", "--root-dir", "/wiremock"]
